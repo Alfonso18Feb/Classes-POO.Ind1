0faab3ea0e4641d36a9bcfe903e88ae3f65d42a7
Alfonso Criado-Pou Castellanos <146075356+Alfonso18Feb@users.noreply.github.com>
2024-03-14 22:34:43 +0100

    Update palindromo.py
    añadir commentarios

--- palindromo.py
+++ palindromo.py
@@ -7,12 +7,14 @@ class Palindromos():
         return self.palabras
     def set_palabras(self, palabras):
         self.palabras=palabras
+    '''Funcion que nos da un booleano deciendo si se cumple o no'''
     def esPalindromo(self):
         palabras = self.palabras.replace(' ','').lower()
         return palabras==palabras[::-1]
+    '''Devuelve el resultado de si es un palindromo con el formato string del atributo'''
     def resultado(self):
         if self.esPalindromo():
-            return f'la palabra es {self.palabras} un palindromo' # f '' es para el formato
+            return f'la palabra es {self.palabras} un palindromo'
         else:
             return f'La palabra {self.palabras} no es un palindromo'
     
